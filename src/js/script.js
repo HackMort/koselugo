207605053ddd6e0078176d3867e7f99a67cb4788
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2023-03-18 16:41:30 -0600

    docs: :memo: Add Description to isiHeaderFixed Funciton.

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,28 @@
-// Document ready vanilla
+// Put your custom javascript here.
 document.addEventListener('DOMContentLoaded', function (event) {
-  console.log('DOM fully loaded and parsed')
-
+  // Listen for scroll event
   window.addEventListener('scroll', () => {
     isiHeaderFixed()
   })
+  // Listen for resize event
   window.addEventListener('resize', () => {
     isiHeaderFixed()
   })
 
+  /**
+   * isiHeaderFixed
+   * @description
+   * - Add class is--fixed to isi__section_header when isi__section is not in viewport
+   * - Add class is--open to isi__section_header when isi__section is not in viewport
+   * - Remove class is--fixed and is--open to isi__section_header when isi__section is in viewport
+   * It requires IntersectionObserver API
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+   * @see https://caniuse.com/#feat=intersectionobserver
+  */
   function isiHeaderFixed () {
     // const mobileMedia = window.matchMedia('(min-width: 768px)')
     const isiHeader = document.querySelector('.isi__section_header')
     const isiSection = document.querySelector('.isi')
-
     const observerOptions = {
       root: null,
       rootMargin: '0px',
